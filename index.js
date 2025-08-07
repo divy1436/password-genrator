fd3edd2c3fec070dc53efced6898c86c3ebee02b
Divyanshu Maurya <2024pceacsdivyanshu58@poornima.org>
2025-08-07 17:38:20 +0530

    using css html js

---
+++ index.js
@@ -0,0 +1,12 @@
+function generatePassword() {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+    const passwordLength = 14;
+    let password = "";
+
+    for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    document.getElementById("password-el").textContent = password;
+}
